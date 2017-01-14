ae068454aa96f27c7f092cd411a6cc51a1540adb
borrys <andrzej.kimel@gmail.com>
2017-01-14 13:54:10 +0100

    added CORS headers

--- src/main/java/coffee/Main.java
+++ src/main/java/coffee/Main.java
@@ -4,6 +4,7 @@ import coffee.events.EventsController;
 import coffee.arrivals.ArrivalsController;
 import coffee.time.CoffeeTimeController;
 import ratpack.guice.Guice;
+import ratpack.http.MutableHeaders;
 import ratpack.server.RatpackServer;
 
 public class Main {
@@ -11,6 +12,12 @@ public class Main {
     RatpackServer.start(server -> server
         .registry(Guice.registry(b -> b.module(CoffeeModule.class)))
         .handlers(chain -> chain
+            .all(ctx -> {
+              MutableHeaders headers = ctx.getResponse().getHeaders();
+              headers.set("Access-Control-Allow-Origin", "*");
+              headers.set("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
+              ctx.next();
+            })
             .path("arrivals", ArrivalsController.class)
             .path("coffeeTime", CoffeeTimeController.class)
             .get("events", EventsController.class)
